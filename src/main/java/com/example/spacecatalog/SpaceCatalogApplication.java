4d0d404e372e834094217ee45c370e3a25603642
Pepi <petranka_popova_@abv.bg>
2024-01-18 20:23:48 +0200

    Initial commit

---
+++ src/main/java/com/example/spacecatalog/SpaceCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.spacecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spacecatalog/SpaceCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spacecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
